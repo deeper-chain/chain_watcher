11af983690e1b0ebab730caf81941b745df6b71e
yubo <anhuitianyu@163.com>
2022-04-07 21:21:32 +0800

    use docker for executing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
 use anyhow::Result;
-use hex_literal::hex;
 use tokio::process::Command;
 //use tokio::sync::mpsc;
-use web3::types::{BlockNumber, FilterBuilder};
+use clap::Parser;
+use hex::FromHex;
+use web3::{
+    ethabi::{self, param_type::ParamType, Token},
+    types::{BlockNumber, FilterBuilder},
+};
 
-// pub enum Message {
-//     CurrentNumber(U64),
-//     Filter(FilterBuilder),
-// }
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(
+        short = 'c',
+        long,
+        default_value = "https://mainnet-dev.deeper.network/rpc"
+    )]
+    chain: String,
+
+    #[clap(short = 'u', long, required = false)]
+    url: Option<String>,
+
+    #[clap(
+        short = 't',
+        long,
+        default_value = "ff68b5ae1c6eef082af114f218b96313f8eaa0e0ccbf5a4d2795eab86b5fdec4"
+    )]
+    topic: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let _ = env_logger::try_init();
+
+    let args = Args::parse();
+    println!("{:?} {}!", args.url, args.chain);
 
     //let output = Command::new("echo").arg("123456").output().await?;
     let output = Command::new("curl")
@@ -21,36 +46,38 @@ async fn main() -> Result<()> {
     let out_string = String::from_utf8_lossy(&output.stdout).to_string();
     println!("shell output {:?}", out_string);
     //let (tx, _rx) = mpsc::unbounded_channel();
-    // loop {
-    let info = out_string.clone();
     let _ = tokio::spawn(async move {
         println!("subscribe begin");
         let _ = subscribe(
-            info,
+            args.chain,
+            args.url.unwrap_or_default(),
+            args.topic,
+            out_string,
             BlockNumber::Number(0.into()),
             BlockNumber::Number(0.into()),
         )
         .await;
     })
     .await;
-
-    //}
-
     Ok(())
 }
 
 pub async fn subscribe(
-    info: String,
+    chain: String,
+    _url: String,
+    topic: String,
+    _out_string: String,
     _from: BlockNumber,
     _to: BlockNumber,
     //rx: mpsc::UnboundedReceiver<Message>,
 ) -> Result<()> {
     //let url= "http://localhost:9933";
-    let url = "https://mainnet-dev.deeper.network/rpc";
-    let topic_hash = hex!("b143df1eb05b3b515daace53f76f3d09c274eaddf108387165a6b64b9c1d40cf");
-    let transport = web3::transports::Http::new(url.clone())?;
+
+    let topic_hash = <[u8; 32]>::from_hex(topic)?;
+    let transport = web3::transports::Http::new(&chain.clone())?;
     let mut web3 = web3::Web3::new(transport);
     let mut base_number = web3.eth().block_number().await?;
+    println!("init block number {:?}", base_number);
     let mut dst_number = base_number;
     loop {
         while base_number >= dst_number {
@@ -60,10 +87,10 @@ pub async fn subscribe(
                 break;
             } else {
                 dst_number = maybe_num.unwrap();
+                println!("now dst block number {:?}", dst_number);
             }
         }
 
-        let info = info.clone();
         let filter = FilterBuilder::default()
             .from_block(BlockNumber::Number(base_number))
             .to_block(BlockNumber::Number(dst_number))
@@ -74,19 +101,35 @@ pub async fn subscribe(
         if let Ok(logs) = logs {
             for log in logs {
                 println!("got log: {:?}", log);
-                let info = info.clone();
-                let _ = tokio::spawn(async {
-                    let res = send_request(info).await;
-                    println!("send_request res {:?}", res);
+                let parse_res =
+                    ethabi::decode(&[ParamType::String, ParamType::String], &log.data.0);
+                let parse_res = parse_res.unwrap_or_default();
+
+                let strs: Vec<String> = parse_res
+                    .into_iter()
+                    .map(|t| match t {
+                        Token::String(res) => res,
+                        _ => "".to_string(),
+                    })
+                    .filter(|s| !s.is_empty())
+                    .collect();
+                if strs.len() != 2 {
+                    continue;
+                }
+
+                //let info = info.clone();
+                let _ = tokio::spawn(async move {
+                    let _ = run_docker(strs[0].clone(), strs[1].clone()).await;
                 })
                 .await;
             }
 
             base_number = dst_number + 1;
         } else {
-            let url = url.clone();
+            let chain = chain.clone();
             loop {
-                let reconn = web3::transports::Http::new(url.clone());
+                let reconn = web3::transports::Http::new(&chain.clone());
+                println!("reconn res {:?}", reconn);
                 if reconn.is_err() {
                     tokio::time::sleep(std::time::Duration::new(1, 0)).await;
                 } else {
@@ -98,7 +141,26 @@ pub async fn subscribe(
     }
 }
 
-pub async fn send_request(sn: String) -> Result<()> {
+pub async fn run_docker(url: String, params: String) -> Result<()> {
+    // if url.is_empty() {
+    //     return Err(anyhow::Error());
+    // }
+    let nurl = url.clone();
+    let names: Vec<&str> = nurl.rsplit_terminator('/').collect();
+    println!("args url {} names0 {:?}", url, names[0]);
+    let output = Command::new("docker").arg("pull").arg(url).output().await?;
+    println!("docker pull res {:?}", output);
+    let output = Command::new("docker")
+        .arg("run")
+        .arg(nurl)
+        .arg(params)
+        .output()
+        .await?;
+    println!("docker run res {:?}", output);
+    Ok(())
+}
+
+pub async fn _send_request(sn: String) -> Result<()> {
     let url = format!("http://81.68.122.162:8000?sn={}", sn);
     let res = reqwest::get(url.clone()).await?.text().await?;
     println!("url {} res {:?}", url, res);
