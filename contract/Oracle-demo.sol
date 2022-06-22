241e482368559b837ac1a96b6ac6517e297f5f71
AmadeusGB <1069830494@qq.com>
2022-06-22 16:30:51 +0800

    update oracle-demo

--- contract/Oracle-demo.sol
+++ contract/Oracle-demo.sol
@@ -2,25 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract Oracle {
-  // Contract owner
-    address public owner;
-
-    // Token MarketPrice Storage
-    uint public tokenMarketPrice;
-
-    // Callback function
+    uint public dotMarketPrice;
     event CallbackGetTokenPrice(uint currentTokenPrice);
 
-    constructor() {
-        owner = msg.sender;
-    }
-
     function setTokenPrice(uint feedPrice) public {
-        tokenMarketPrice = feedPrice;
-        emit CallbackGetTokenPrice(tokenMarketPrice);
-    }
-
-    function getTokenPrice() view public returns (uint) {
-        return tokenMarketPrice;
+        dotMarketPrice = feedPrice;
+        emit CallbackGetTokenPrice(dotMarketPrice);
     }
 }
