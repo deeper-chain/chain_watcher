3c0ab77f10584dcec04114864ab8dbd11e7adc7e
AmadeusGB <1069830494@qq.com>
2022-10-19 11:42:25 +0800

    fmt

--- contract/Dapp-template.sol
+++ contract/Dapp-template.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.8.0;
 
 interface IDEP{
 	function nNodespecifiedAddressTask(
-	        string calldata url, 
-			string calldata options, 
-			uint64 maxRunNum, 
-			address[] memory receivers, 
-			uint64 maintainBlocks
-	    ) external;
-    function blockUintPrice() external returns(uint64);
+		string calldata url, 
+		string calldata options, 
+		uint64 maxRunNum, 
+		address[] memory receivers, 
+		uint64 maintainBlocks
+	) external;
+	function blockUintPrice() external returns(uint64);
 	function proofUnit() external returns(uint256);
 }
 
 
 interface IERC20 {
-    function transferFrom(address from, address to, uint256 amount) external;
+	function transferFrom(address from, address to, uint256 amount) external;
 }
 
 contract DepTaskBridge {
@@ -27,25 +27,25 @@ contract DepTaskBridge {
 	address public owner;
 	IDEP public dep;
 	Task public currentWork;
-    IERC20 ezc;
+	IERC20 ezc;
 
 
 	event NewTaskChange(string url, uint256 time);
 
 	modifier onlyOwner{
-		require(msg.sender == owner);
-        _;
+	require(msg.sender == owner);
+		_;
 	}	
 
 	constructor(address _dep, address _ezc) {
 		owner = msg.sender;
 		dep = IDEP(_dep);
-        IERC20 ezc = IERC20(_ezc);
+		IERC20 ezc = IERC20(_ezc);
 	}
 
 	function setDEP(IDEP _dep) external onlyOwner {
 		dep = _dep;
-    }
+	}
 
 	function setTask(string calldata workURL, string calldata options) external onlyOwner{
 		currentWork.url = workURL;
@@ -54,8 +54,8 @@ contract DepTaskBridge {
 	}
 
 	function payForTask(address[] memory user_address, uint64 maintainBlocks) external{
-        uint256 totalPrice = calcEZC(1, maintainBlocks);
-        ezc.transferFrom(msg.sender, address(this),totalPrice);
+		uint256 totalPrice = calcEZC(1, maintainBlocks);
+		ezc.transferFrom(msg.sender, address(this),totalPrice);
 		dep.nNodespecifiedAddressTask(currentWork.url, currentWork.options, 1, user_address, maintainBlocks);
 	}
 
@@ -63,9 +63,9 @@ contract DepTaskBridge {
 		uint64  blockUintPrice = 100;
 		uint256 proofUnit = 1 ether;
 		uint256 taskTotalPrice = 0;
-        uint64 blockPrice = 1;
+		uint64 blockPrice = 1;
 		if (maintainBlocks > 100) blockPrice = maintainBlocks / blockUintPrice;
-        
-        taskTotalPrice = proofUnit * maxRunNum * blockPrice;
+
+		taskTotalPrice = proofUnit * maxRunNum * blockPrice;
 	}
 }
